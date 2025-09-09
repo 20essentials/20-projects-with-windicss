cf84cb963fa082bc3348ac4a91ee9eb755fc92f5
DominicDev <s0l0estudi4r@gmail.com>
2025-09-09 13:39:27 +0200

    v0.0.1

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Foto Preview](preview/20-projects-with-windicss.avif)](https://20essentials.github.io/20-projects-with-windicss)
+
---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  build: {
+    assets: 'static'
+  },
+  
+  //Localhost
+  // site: 'http://localhost:4321/'
+
+  //Deploy normal project
+  // base: '/project-1020/',
+  // site: 'https://20essentials.github.io/project-1020/'
+
+  //Deploy 20 Projects
+  base: '/20-projects-with-windicss/',
+  site: 'https://20essentials.github.io/20-projects-with-windicss/',
+});
